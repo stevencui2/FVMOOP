d03b58da1dd012f2845a28f314e542df1e9df1c7
stevencui2 <liang.cui.95@gmail.com>
2021-03-22 15:18:34 -0300

    remove visual studio and add makefile

---
+++ src/createFiniteMatrices.h
@@ -0,0 +1,9 @@
+FiniteMatrix::finiteMat AE(NI,vector<FiniteMatrix>(NJ));
+FiniteMatrix::finiteMat AW(NI,vector<FiniteMatrix>(NJ));
+FiniteMatrix::finiteMat AS(NI,vector<FiniteMatrix>(NJ));
+FiniteMatrix::finiteMat AN(NI,vector<FiniteMatrix>(NJ));
+FiniteMatrix::finiteMat AP(NI,vector<FiniteMatrix>(NJ));
+FiniteMatrix::finiteMat SU(NI,vector<FiniteMatrix>(NJ));
+FiniteMatrix::finiteMat SV(NI,vector<FiniteMatrix>(NJ));
+FiniteMatrix::finiteMat APU(NI,vector<FiniteMatrix>(NJ));
+FiniteMatrix::finiteMat APV(NI,vector<FiniteMatrix>(NJ));
